01de6812a56b2a54d2c860906bdb26052cf040d4
LiviaGeorgiana <livia.staicutoiu@gmail.com>
2017-06-01 22:08:32 +0300

    Init

---
+++ app/src/main/java/com/example/livia/iswint/LogIn.java
@@ -0,0 +1,113 @@
+package com.example.livia.iswint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LogIn extends Activity {
+
+    private EditText mEmailFiled;
+    private  EditText mPasswordField;
+
+    private Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_log_in);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailFiled = (EditText) findViewById(R.id.email_field);
+        mPasswordField = (EditText) findViewById(R.id.password_field);
+
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(LogIn.this, account_activity.class));
+                }
+            }
+        };
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startSingIn();;
+
+            }
+
+            public void OnClick(View view){
+
+            }
+        });
+
+
+    }
+    protected  void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSingIn() {
+        String email = mEmailFiled.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+
+            Toast.makeText(LogIn.this, "Fields are empty.", Toast.LENGTH_LONG).show();
+
+
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if(!task.isSuccessful()) {
+                        Toast.makeText(LogIn.this, "Sing In Problem", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        }
+
+
+    }
+}
+
+
